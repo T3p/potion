996325315d247b8bb370cee4ed71bce50b059907
Matteo <matteo.papini@polimi.it>
2019-06-19 15:08:14 +0200

    pre-cleanup

--- potion/simulation/play.py
+++ potion/simulation/play.py
@@ -5,6 +5,9 @@
 """
 
 from potion.simulation.trajectory_generators import generate_batch
+from potion.common.misc_utils import clip
 
-def play(env, policy, horizon=100, episodes=100):
-    generate_batch(env, policy, horizon, episodes, render=True)
+def play(env, policy, horizon=100, episodes=100, render=True, action_filter=None):
+    if action_filter is None:
+        action_filter = clip(env)
+    return generate_batch(env, policy, horizon, episodes, render=render, action_filter=action_filter)
