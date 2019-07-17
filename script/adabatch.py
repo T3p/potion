d1ba886d0cd9a803f576cba4dd0b74bdf227422b
Matteo <matteo.papini@polimi.it>
2019-07-17 13:26:41 +0200

    ..

--- script/adabatch.py
+++ script/adabatch.py
@@ -39,7 +39,7 @@ parser.add_argument('--max_samples', help='Maximum total samples', type=int,
 parser.add_argument('--min_batchsize', help='(Minimum) batch size', type=int, 
                     default=100)
 parser.add_argument('--max_batchsize', help='Maximum batch size', type=int, 
-                    default=10000)
+                    default=50000)
 parser.add_argument('--disc', help='Discount factor', type=float, default=0.9)
 parser.add_argument('--conf', help='Confidence', type=float, default=0.95)
 parser.add_argument('--std_init', help='Initial policy std', type=float,
