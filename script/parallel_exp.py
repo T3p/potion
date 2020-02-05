19f1832a9cde5c7ba8e378efe0e1aeabe3a73697
Matteo <matteo.papini@polimi.it>
2020-02-05 11:40:17 +0100

    ..

--- script/parallel_exp.py
+++ script/parallel_exp.py
@@ -12,10 +12,10 @@ import screenutils as su
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--command', help='Command', type=str, default='echo hello')
 parser.add_argument('--name', help='Name', type=str, default='hello')
-parser.add_argument('--nseeds', help='Number of seeds', type=int, default=3)
+parser.add_argument('--nseeds', help='Number of seeds', type=int, default=5)
 args = parser.parse_args()
 
-seeds = [951, 160, 178]
+seeds = [265, 424, 726, 173, 711]
 
 for seed in seeds[:args.nseeds]:    
     screen = su.Screen(args.name + '_' + str(seed), create=True)
