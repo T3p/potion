b911d10dd7ba7e55e89a1ae86c79cb4f181ee1d4
Matteo <matteo.papini@polimi.it>
2020-02-07 10:58:53 +0100

    updated dpo notebook

--- potion/simulation/play.py
+++ potion/simulation/play.py
@@ -7,7 +7,7 @@
 from potion.simulation.trajectory_generators import generate_batch
 from potion.common.misc_utils import clip
 
-def play(env, policy, horizon=100, episodes=100, render=True, action_filter=None):
+def play(env, policy, horizon=100, episodes=100, render=True, action_filter=None, fast=False):
     if action_filter is None:
         action_filter = clip(env)
     return generate_batch(env, policy, horizon, episodes, render=render, action_filter=action_filter)
