fixed adam optimizer; gaussian policy now takes action range as input; tested gpomdp and variance-reduced extensions with deep policies on mujoco's swimmer

potion/actors/continuous_policies.py
@@ -172,33 +172,33 @@ class DeepGaussianPolicy(ContinuousPolicy):
     Factored
     MLP mean \mu_{\theta}(x)
     diagonal, state-independent std \sigma = e^{\omega}
-    N.B. the squashing function is NOT considered in gradients
     """
     def __init__(self, n_states, n_actions, 
                  hidden_neurons=[], 
-                 feature_fun=None, 
-                 squash_fun=None,
+                 state_preproc=None, 
                  mu_init=None, 
                  logstd_init=None, 
                  learn_std=True,
                  bias=False,
                  activation=torch.tanh,
-                 init=torch.nn.init.xavier_uniform_):
+                 init=torch.nn.init.xavier_uniform_,
+                 action_range=None):
         super(DeepGaussianPolicy, self).__init__()
         self.n_states = n_states
         self.n_actions = n_actions
-        self.feature_fun = feature_fun
-        self.squash_fun = squash_fun
+        self.preproc = state_preproc
         self.learn_std = learn_std
         self.mu_init = mu_init
         self.logstd_init = logstd_init
+        self.action_range = action_range
         
         # Mean
         self.mu = MLPMapping(n_states, n_actions, 
                              hidden_neurons, 
                              bias, 
                              activation, 
-                             init)
+                             init,
+                             self.action_range)
         if mu_init is not None:
             self.mu.set_from_flat(mu_init)
         
@@ -219,8 +219,8 @@ class DeepGaussianPolicy(ContinuousPolicy):
     def log_pdf(self, s, a):
         log_sigma = self.logstd
         sigma = torch.exp(log_sigma)
-        if self.feature_fun is not None:
-            x = self.feature_fun(s)
+        if self.preproc is not None:
+            x = self.preproc(s)
         else:
             x = s
         logp = -((a - self.mu(x)) ** 2) / (2 * sigma ** 2) - \
@@ -232,19 +232,17 @@ class DeepGaussianPolicy(ContinuousPolicy):
     
     def act(self, s, deterministic=False):
         with torch.no_grad():
-            if self.feature_fun is not None:
-                x = self.feature_fun(s)
+            if self.preproc is not None:
+                x = self.preproc(s)
             else:
                 x = s
-            
+            a = self.mu(x)
+
             if deterministic:
-                return self.mu(x)
+                return a
             
             sigma = torch.exp(self.logstd.data)
-            a = self.mu(x) + self._pdf.sample() * sigma
-            if self.squash_fun is not None:
-                a = self.squash_fun(a)
-            return a
+            return a + self._pdf.sample() * sigma
         
     def num_loc_params(self):
         return self.mu.num_params()
@@ -290,8 +288,8 @@ class DeepGaussianPolicy(ContinuousPolicy):
                 'ActionDim': self.n_actions,
                 'MuInit': self.mu_init,
                 'LogstdInit': self.logstd_init,
-                'FeatureFun': self.feature_fun,
-                'SquashFun': self.squash_fun}
+                'StatePreprocFun': self.preproc,
+                'ActionRange': self.action_range}
         
         
 class UniformPolicy(ContinuousPolicy):
@@ -486,143 +484,6 @@ class ShallowSquashedPolicy(ContinuousPolicy):
                 'LogstdInit': self.logstd_init,
                 'FeatureFun': self.feature_fun}
 
-
-class DeepSquashedPolicy(ContinuousPolicy):
-    """
-    Composition of (factored) Gaussian with tanh
-    MLP mean \mu_{\theta}(x)
-    diagonal, state-independent std \sigma = e^{\omega}
-    """
-    def __init__(self, n_states, n_actions, 
-                 hidden_neurons=[], 
-                 feature_fun=None, 
-                 shift = None,
-                 scale = None,
-                 mu_init=None, 
-                 logstd_init=None, 
-                 learn_std=True,
-                 bias=False,
-                 activation=torch.tanh,
-                 init=torch.nn.init.xavier_uniform_):
-        super(DeepSquashedPolicy, self).__init__()
-        self.n_states = n_states
-        self.n_actions = n_actions
-        self.feature_fun = feature_fun
-        self.shift = torch.zeros(self.n_actions) if shift is None else tu.maybe_tensor(shift)
-        self.scale = torch.ones(self.n_actions) if scale is None else tu.maybe_tensor(scale)
-        self.learn_std = learn_std
-        self.mu_init = mu_init
-        self.logstd_init = logstd_init
-        
-        # Mean
-        self.mu = MLPMapping(n_states, n_actions, 
-                             hidden_neurons, 
-                             bias, 
-                             activation, 
-                             init)
-        if mu_init is not None:
-            self.mu.set_from_flat(mu_init)
-        
-        # Log of standard deviation
-        if logstd_init is None:
-            logstd_init = torch.zeros(self.n_actions)
-        elif not torch.is_tensor(logstd_init):
-            logstd_init = torch.tensor(logstd_init)
-        if learn_std:
-            self.logstd = nn.Parameter(logstd_init)
-        else:
-            self.logstd = autograd.Variable(logstd_init)
-        
-        # Normal(0,1)
-        self._pdf = Normal(torch.zeros_like(self.logstd.data), 
-                               torch.ones(n_actions))
-
-    def log_pdf(self, s, a):
-        log_sigma = self.logstd
-        sigma = torch.exp(log_sigma)
-        if self.feature_fun is not None:
-            x = self.feature_fun(s)
-        else:
-            x = s
-        
-        #Change of variable
-        a_bar = (a - self.shift) / self.scale
-        u = tu.atanh(a_bar)
-        
-        logp = -((u - self.mu(x)) ** 2) / (2 * sigma ** 2) - \
-            log_sigma  - .5 * math.log(2 * math.pi) - \
-            torch.log(torch.abs(1 - a_bar**2)) - torch.log(torch.abs(self.scale))
-
-        return torch.sum(logp, -1)
-    
-    def forward(self, s, a):
-        return torch.exp(self.log_pdf(s, a))
-    
-    def act(self, s, deterministic=False):
-        with torch.no_grad():
-            if self.feature_fun is not None:
-                x = self.feature_fun(s)
-            else:
-                x = s
-            
-            if deterministic:
-                return self.scale * torch.tanh(self.mu(x)) + self.shift
-            
-            sigma = torch.exp(self.logstd.data)
-            u = self.mu(x) + self._pdf.sample() * sigma
-            
-            #Squashing
-            a = self.scale * torch.tanh(u) + self.shift
-            
-            return a
-        
-    def num_loc_params(self):
-        return self.mu.num_params()
-    
-    def num_scale_params(self):
-        return self.n_actions
-    
-    def get_loc_params(self):
-        return self.mu.get_flat()
-    
-    def get_scale_params(self):
-        return self.logstd.data
-    
-    def set_loc_params(self, val):
-        self.mu.set_from_flat(val)
-        
-    def set_scale_params(self, val):
-        with torch.no_grad():
-            self.logstd.data = torch.tensor(val)
-    
-    def exploration(self):
-        return torch.exp(torch.sum(self.logstd)).data
-    
-    def entropy(self, s):
-        s = tu.complete_out(s, 3)
-        ent = torch.sum(self.logstd) + \
-                1./(2 * self.n_actions) * (1 + math.log(2 * math.pi))
-        return torch.zeros(s.shape[:-1]) + ent
-
-    def entropy_grad(self, s):
-        with torch.no_grad():
-            s = tu.complete_out(s, 3)
-            if self.learn_std:
-                return torch.cat((torch.ones(s.shape[:-1] + (self.n_actions,)), 
-                              torch.zeros(s.shape[:-1] + (self.n_states,))), -1)
-            else:
-                return torch.zeros(s.shape[:-1] + (self.n_states,))
-            
-    def info(self):
-        return {'PolicyDlass': self.__class__.__name__,
-                'LearnStd': self.learn_std,
-                'StateDim': self.n_states,
-                'ActionDim': self.n_actions,
-                'MuInit': self.mu_init,
-                'LogstdInit': self.logstd_init,
-                'FeatureFun': self.feature_fun}
-
-
 """
 Testing
 """
@@ -651,37 +512,3 @@ if __name__ == '__main__':
     print(dp.num_loc_params())
     print(dp.act(a))
     print()
-    
-    #Testing linear squashed policy
-    feat = None
-    s = 0.1 + torch.zeros(ds)
-    a = 0.1 + torch.zeros(da)
-    mu_init = 0.1 + torch.zeros(ds*da)
-    shift = -1.
-    scale = 2.
-    learn_std = True
-    sp = ShallowSquashedPolicy(ds, da, feat, shift, scale, mu_init, 
-                               learn_std=learn_std, 
-                               logstd_init=torch.zeros(da))
-    print(sp.act(s))
-    print(sp(s,a))
-    print(sp.log_pdf(s,a))
-    print(tu.flat_gradients(sp, sp.log_pdf(s,a)))
-    print(sp.loc_score(s, a))
-    print(sp.score(s, a))
-    
-    #Testing deep squashed policy
-    feat = None
-    s = 0.1 + torch.zeros(ds)
-    a = 0.1 + torch.zeros(da)
-    mu_init = None
-    shift = -1.
-    scale = 2.
-    learn_std = True
-    dsp = DeepSquashedPolicy(ds, da, [2,4], feat, shift, scale, mu_init, 
-                               learn_std=learn_std, 
-                               logstd_init=torch.zeros(da))
-    print(dsp.act(s))
-    print(dsp(s,a))
-    print(dsp.log_pdf(s,a))
-    print(tu.flat_gradients(dsp, dsp.log_pdf(s,a)))

potion/algorithms/reinforce.py
@@ -189,7 +189,7 @@ def reinforce(env, policy, horizon, *,
             grad_var = torch.sum(torch.diag(grad_cov)).item() #for humans
         else:
             grad = grad_samples
-        
+                    
         if verbose > 1:
             print('Gradients: ', grad)
         log_row['GradNorm'] = torch.norm(grad).item()

potion/common/mappings.py
@@ -33,7 +33,8 @@ class MLPMapping(tu.FlatModule):
     def __init__(self, d_in, d_out, hidden_neurons, 
                  bias=False, 
                  activation=torch.tanh, 
-                 init=nn.init.xavier_uniform_):
+                 init=nn.init.xavier_uniform_,
+                 output_range=None):
         """
         Multi-layer perceptron
         
@@ -49,20 +50,36 @@ class MLPMapping(tu.FlatModule):
         
         self.hidden_layers = []
         input_size = self.d_in
-        for i, depth in enumerate(hidden_neurons):
-            layer = nn.Linear(input_size, depth, bias)
+        for i, width in enumerate(hidden_neurons):
+            layer = nn.Linear(input_size, width, bias)
             init(layer.weight)
             self.add_module("hidden"+str(i), layer)
             self.hidden_layers.append(layer)
-            input_size = depth
+            input_size = width
         self.last = nn.Linear(input_size, self.d_out, bias)
         init(self.last.weight)
         self.add_module("last", self.last)
+        #output layer
+        if output_range is None:
+            self.out = None
+        elif type(output_range)==float:
+            assert output_range > 0
+            self.out = lambda x: torch.tanh(x) * output_range #[-c, c]
+        elif type(output_range)==tuple:
+            assert len(output_range)==2
+            lower, upper = output_range
+            assert upper > lower
+            self.out = lambda x: (1 + torch.tanh(x)) * (upper - lower) / 2 + lower
+        else:
+            raise NotImplementedError("Supported ranges: float (-x, x) or tuple (lower, upper)")
         
     def forward(self, x):
         for linear in self.hidden_layers:
             x = self.activation(linear(x))
-        return self.last(x) #final linear mapping
+        if self.out is not None:
+            return self.out(self.last(x))
+        else:
+            return self.last(x)
     
 """
 Testing

potion/estimation/offpolicy_gradients.py
@@ -11,7 +11,6 @@ from potion.common.misc_utils import unpack, discount
 from potion.common.torch_utils import tensormat, jacobian
 from potion.estimation.moments import incr_mean, incr_var
 
-
 def off_gpomdp_estimator(batch, disc, policy, target_params, 
                          baselinekind='avg', 
                          result='mean',
@@ -33,16 +32,14 @@ def off_gpomdp_estimator(batch, disc, policy, target_params,
         for shallow policies)
     """
     if shallow:
-        return _shallow_off_gpomdp_estimator(batch, disc, policy, target_params, baselinekind, result)
+        return _shallow_off_gpomdp_estimator(batch, disc, policy, target_params,
+                                             baselinekind, result)
     else:
-        raise NotImplementedError
-
-def _shallow_off_gpomdp_estimator(batch, disc, policy, target_params, 
-                                  baselinekind, 
-                                  result):
-    with torch.no_grad():        
-        states, actions, rewards, mask, _ = unpack(batch) # NxHxm, NxHxd, NxH, NxH
-        
+        #Data
+        N = len(batch)
+        states, actions, rewards, mask, _ = unpack(batch) #NxHxd_s, NxHxd_a, NxH, NxH
+        H = rewards.shape[1]
+        m = policy.num_params()
         disc_rewards = discount(rewards, disc) #NxH
         
         #Behavioral
@@ -51,46 +48,97 @@ def _shallow_off_gpomdp_estimator(batch, disc, policy, target_params,
         
         #Target
         policy.set_from_flat(target_params)
-        scores = policy.score(states, actions) #NxHxM
-        G = torch.cumsum(tensormat(scores, mask), 1) #NxHxm
-        n_k = torch.sum(mask, dim=0) #H
-        n_k[n_k==0.] = 1.
-        target_logps = policy.log_pdf(states, actions) #NxH
+        target_logps = policy.log_pdf(states, actions) * mask #NxH
+        cm_logps = torch.cumsum(target_logps, 1) #NxH
+        
+        log_iws = torch.cumsum((target_logps - behavioral_logps) * mask, 1) #NxH
+        stabilizers, _ = torch.max(log_iws, dim=0, keepdim=True) #NxH
+        
+        if baselinekind == 'peters':
+            jac = jacobian(policy, cm_logps.view(-1)).reshape((N,H,m)) #NxHxm   
+            b_num = torch.sum(tensormat((jac * torch.exp(log_iws - stabilizers).unsqueeze(1))**2, 
+                                        disc_rewards), 0) #Hxm
+            b_den = torch.sum((jac * torch.exp(log_iws - stabilizers).unsqueeze(1))**2, 0) #Hxm
+            baseline = b_num / b_den #Hxm
+            baseline[baseline != baseline] = 0
+            values = disc_rewards.unsqueeze(2) - baseline.unsqueeze(0) #NxHxm
+            _samples = torch.sum(tensormat(values * torch.exp(log_iws).unsqueeze(1) * jac, mask), 1) #Nxm
+        else:
+            if baselinekind == 'avg':
+                baseline = torch.mean(disc_rewards, 0) #H
+            else:
+                baseline = torch.zeros(1) #1
+            values = (disc_rewards - baseline) * mask #NxH
+            
+            _samples = torch.stack([tu.flat_gradients(policy, cm_logps[i,:], 
+                                                  values[i,:] * torch.exp(log_iws)[i,:])
+                                           for i in range(N)], 0) #Nxm
         
         #restore (behavioral) policy
         policy.set_from_flat(behavioral_params)
+        if result == 'samples':
+            return _samples #Nxm
+        else:
+            return torch.mean(_samples, 0) #m
+
+def _shallow_off_gpomdp_estimator(batch, disc, policy, target_params, 
+                         baselinekind, result):
+    with torch.no_grad():
+        states, actions, rewards, mask, _ = unpack(batch) # NxHxm, NxHxd, NxH, NxH
+        disc_rewards = discount(rewards, disc) #NxH
         
-        log_iws = torch.cumsum(target_logps - behavioral_logps, 1) #NxH
-        stabilizers, _ = torch.max(log_iws, dim=1, keepdim=True) #NxH
+        #Behavioral
+        behavioral_params = policy.get_flat()
+        behavioral_logps = policy.log_pdf(states, actions) #NxH
+        
+        #Target
+        policy.set_from_flat(target_params)
+        target_logps = policy.log_pdf(states, actions) #NxH
+        scores = policy.score(states, actions) #NxHxM
+        
+        #restore (behavioral) policy
+        policy.set_from_flat(behavioral_params)
+
+        G = torch.cumsum(tensormat(scores, mask), 1) #NxHxm
+        n_k = torch.sum(mask, dim=0) #H
+        n_k[n_k==0.] = 1.
+              
+        log_iws = torch.cumsum((target_logps - behavioral_logps) * mask, 1) #NxH
+        stabilizers, _ = torch.max(log_iws, dim=0, keepdim=True) #NxH
         
         if baselinekind == 'peters': #Mastrangelo's variant
             baseline = torch.sum(tensormat(G ** 2, disc_rewards * torch.exp(2*(log_iws - stabilizers))), 0) / \
                             torch.sum(tensormat(G ** 2, torch.exp(2*(log_iws - stabilizers))), 0) #Hxm
-        elif baselinekind == 'zero':
-            baseline = torch.zeros_like(G[0]) #Hxm
+            baseline[baseline != baseline] = 0 #removes non-real values
+            values = disc_rewards.unsqueeze(2) - baseline.unsqueeze(0) #NxHxm
         else:
-            raise NotImplementedError
-        baseline[baseline != baseline] = 0 #removes non-real values
-        values = disc_rewards.unsqueeze(2) - baseline.unsqueeze(0) #NxHxm
+            if baselinekind == 'zero':
+                baseline = torch.zeros_like(disc_rewards[0]) #H
+            elif baselinekind == 'avg':
+                baseline = torch.mean(disc_rewards, 0) #H
+            else:
+                raise NotImplementedError
+            baseline[baseline != baseline] = 0 #removes non-real values
+            values = (disc_rewards - baseline.unsqueeze(0)).unsqueeze(2) #NxHxm     
         
         _samples = torch.sum(tensormat(G * values, mask * torch.exp(log_iws)), 1) #Nxm
         if result == 'samples':
             return _samples #Nxm
         else:
             return torch.mean(_samples, 0) #m
-    
 
-#entropy-augmented version
-def egpomdp_estimator(batch, disc, policy, coeff, baselinekind='avg', result='mean',
-                     shallow=False):
-    """G(PO)MDP policy gradient estimator
+def off_reinforce_estimator(batch, disc, policy, target_params, 
+                         baselinekind='avg', 
+                         result='mean',
+                         shallow=False):
+    """off-policy reinforce policy gradient estimator
        
-    batch: list of N trajectories. Each trajectory is a tuple 
+    batch: list of N trajectories generated by behavioral policy. Each trajectory is a tuple 
         (states, actions, rewards, mask). Each element of the tuple is a 
         tensor where the first dimension is time.
     disc: discount factor
-    policy: the one used to collect the data
-    coeff: entropy bonus coefficient
+    policy: behavioral policy, the one used to collect the data
+    target_params: parameters of the policy to evaluate
     baselinekind: kind of baseline to employ in the estimator. 
         Either 'avg' (average reward, default), 'peters' 
         (variance-minimizing),  or 'zero' (no baseline)
@@ -100,43 +148,106 @@ def egpomdp_estimator(batch, disc, policy, coeff, baselinekind='avg', result='me
         for shallow policies)
     """
     if shallow:
-        return _shallow_egpomdp_estimator(batch, disc, policy, coeff, baselinekind, result)    
+        return _shallow_off_reinforce_estimator(batch, disc, policy, target_params, 
+                                 baselinekind, result)
     else:
-        raise NotImplementedError
-
-
-def off_reinforce_estimator(batch, disc, policy, baselinekind='avg', 
-                        result='mean', shallow=False):
-    """REINFORCE policy gradient estimator
-       
-    batch: list of N trajectories. Each trajectory is a tuple 
-        (states, actions, rewards, mask). Each element of the tuple is a 
-        tensor where the first dimension is time.
-    disc: discount factor
-    policy: the one used to collect the data
-    baselinekind: kind of baseline to employ in the estimator. 
-        Either 'avg' (average reward, default), 'peters' 
-        (variance-minimizing),  or 'zero' (no baseline)
-    result: whether to return the final estimate ('mean', default), or the 
-        single per-trajectory estimates ('samples')
-    shallow: whether to use precomputed score functions (only available
-        for shallow policies)
-    """
-    raise NotImplementedError
-
-
-#entopy-augmented version     
-def _shallow_egpomdp_estimator(batch, disc, policy, coeff, baselinekind='peters', result='mean'):
-    raise NotImplementedError
-
+        #data
+        N = len(batch)
+        states, actions, rewards, mask, _ = unpack(batch) #NxHxm, NxHxd, NxH, NxH
         
-def _shallow_reinforce_estimator(batch, disc, policy, baselinekind='peters', result='mean'):
-    raise NotImplementedError
+        disc_rewards = discount(rewards, disc) #NxH
+        rets = torch.sum(disc_rewards, 1) #N
+        
+        #Behavioral
+        behavioral_params = policy.get_flat()
+        behavioral_logps = policy.log_pdf(states, actions) #NxH
+        
+        #Target
+        policy.set_from_flat(target_params)
+        target_logps = policy.log_pdf(states, actions) * mask #NxH
+    
+        log_iws = torch.sum((target_logps - behavioral_logps) * mask, 1) #N
+        stabilizers, _ = torch.max(log_iws, dim=0, keepdim=True) #N
+        
+        if baselinekind == 'peters':
+            logp_sums = torch.sum(target_logps, 1) #N
+            jac = jacobian(policy, logp_sums) #Nxm   
+            b_num = torch.sum((jac * torch.exp(log_iws - stabilizers).unsqueeze(1))**2 * rets.unsqueeze(1), 0) #m
+            b_den = torch.sum((jac * torch.exp(log_iws - stabilizers).unsqueeze(1))**2, 0) #m
+            baseline = b_num / b_den #m
+            baseline[baseline != baseline] = 0
+            values = rets.unsqueeze(1) - baseline.unsqueeze(0) #Nxm
+            _samples = jac * values * torch.exp(log_iws).unsqueeze(1)
+        else:
+            if baselinekind == 'avg':
+                baseline = torch.mean(rets, 0) #1
+            else:
+                baseline = torch.zeros(1) #1
+            baseline[baseline != baseline] = 0
+            values = rets - baseline #N
+            
+            if result == 'mean':
+                logp_sums = torch.sum(target_logps, 1)
+                grad = tu.flat_gradients(policy, logp_sums, values * torch.exp(log_iws)) / N
+                #restore (behavioral) policy
+                policy.set_from_flat(behavioral_params)
+                return grad
+            
+            _samples = torch.stack([tu.flat_gradients(policy, target_logps[i,:]) * 
+                                                         values[i] * torch.exp(log_iws)[i]
+                                           for i in range(N)], 0) #Nxm
+        
+        #restore (behavioral) policy
+        policy.set_from_flat(behavioral_params)
+        if result == 'samples':
+            return _samples #Nxm
+        else:
+            return torch.mean(_samples, 0) #m
 
+def _shallow_off_reinforce_estimator(batch, disc, policy, target_params, 
+                         baselinekind, result):
+    with torch.no_grad():
+        states, actions, rewards, mask, _ = unpack(batch) # NxHxm, NxHxd, NxH, NxH
+        disc_rewards = discount(rewards, disc) #NxH 
+        returns = torch.sum(disc_rewards, 1) #N
+        
+        #Behavioral
+        behavioral_params = policy.get_flat()
+        behavioral_logps = policy.log_pdf(states, actions) #NxH
+        
+        #Target
+        policy.set_from_flat(target_params)
+        target_logps = policy.log_pdf(states, actions) #NxH
+        scores = policy.score(states, actions) #NxHxM
+        
+        #restore (behavioral) policy
+        policy.set_from_flat(behavioral_params)        
 
-def _incr_shallow_gpomdp_estimator(traj, disc, policy, baselinekind='peters', result='mean', cum_1 = 0., cum_2 = 0., cum_3 = 0., tot_trajs = 1):
-    raise NotImplementedError
-
+        G = torch.sum(tensormat(scores, mask), 1) #Nxm
+        
+        log_iws = torch.sum((target_logps - behavioral_logps) * mask, 1) #N
+        stabilizers, _ = torch.max(log_iws, dim=0, keepdim=True) #N
+        
+        if baselinekind == 'peters': #Mastrangelo's variant
+            baseline = torch.sum(returns.unsqueeze(1) * (torch.exp(log_iws - stabilizers).unsqueeze(1) * G)**2, 0) / \
+                            torch.sum((torch.exp(log_iws - stabilizers).unsqueeze(1) * G)**2, 0) #m
+            baseline[baseline != baseline] = 0 #removes non-real values
+            values = returns.unsqueeze(1) - baseline.unsqueeze(0) #Nxm
+        else:
+            if baselinekind == 'zero':
+                baseline = torch.zeros(1) #1
+            elif baselinekind == 'avg':
+                baseline = torch.mean(returns, 0) #1
+            else:
+                raise NotImplementedError
+            baseline[baseline != baseline] = 0 #removes non-real values
+            values = (returns - baseline.unsqueeze(0)).unsqueeze(1) #Nxm     
+        
+        _samples = torch.exp(log_iws).unsqueeze(1) * values * G #Nxm
+        if result == 'samples':
+            return _samples #Nxm
+        else:
+            return torch.mean(_samples, 0) #m        
         
 """Testing"""
 if __name__ == '__main__':

potion/meta/steppers.py
@@ -82,7 +82,9 @@ class Adam:
         self.v = self.beta2 * self.v + (1 - self.beta2) * grad**2
         m_hat = self.m / (1 - self.beta1**self.t)
         v_hat = self.v / (1 - self.beta2**self.t)
-        return self.alpha / (torch.sqrt(v_hat) + self.epsilon) * m_hat
+        step = self.alpha / (torch.sqrt(v_hat) + self.epsilon) * m_hat / grad #division by grad needed since adam does not compute a step but an update vector!
+        step[step != step] = 0. #where grad is zero we can safely set the step to zero
+        return step
     
     def reset(self):
         self.m = 0

potion/simulation/trajectory_generators.py
@@ -46,11 +46,11 @@ def sequential_episode_generator(env, policy, horizon=float('inf'), max_episodes
             if not torch.is_tensor(a):
                 a = torch.tensor(a)
             a = a.view(-1)
-            if action_filter is not None:
-                a = action_filter(a)
             _a = a.numpy()
             if len(a)==1:
                 _a = _a.item()
+            if action_filter is not None:
+                _a = action_filter(_a)
             next_s, r, done, info = env.step(_a)
             if render:
                 try:

script/test_deep_gaussian.py
@@ -8,29 +8,33 @@ Created on Wed Jan 16 14:47:33 2019
 import torch
 import gym
 import potion.envs
-from potion.actors.continuous_policies import ShallowGaussianPolicy
-from potion.actors.discrete_policies import ShallowGibbsPolicy
+from potion.actors.continuous_policies import DeepGaussianPolicy
 from potion.common.logger import Logger
 from potion.algorithms.reinforce import reinforce
+from potion.algorithms.variance_reduced import svrpg, srvrpg, stormpg, pagepg
 import argparse
 import re
 from potion.meta.steppers import ConstantStepper, RMSprop, Adam
 from gym.spaces.discrete import Discrete
+import numpy as np
+from functools import partial
 
 # Command line arguments
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument('--name', help='Experiment name', type=str, default='GPOMDP')
+parser.add_argument('--name', help='Experiment name', type=str, default='DEEP_TEST')
 parser.add_argument('--estimator', help='Policy gradient estimator (reinforce/gpomdp)', type=str, default='gpomdp')
-parser.add_argument('--baseline', help='baseline for policy gradient estimator (avg/peters/zero)', type=str, default='peters')
+parser.add_argument('--baseline', help='baseline for policy gradient estimator (avg/peters/zero)', type=str, default='avg')
 parser.add_argument('--seed', help='RNG seed', type=int, default=0)
-parser.add_argument('--env', help='Gym environment id', type=str, default='LQ-v0')
-parser.add_argument('--horizon', help='Task horizon', type=int, default=10)
-parser.add_argument('--batchsize', help='Initial batch size', type=int, default=100)
-parser.add_argument('--iterations', help='Iterations', type=int, default=100)
-parser.add_argument('--disc', help='Discount factor', type=float, default=0.9)
+parser.add_argument('--env', help='Gym environment id', type=str, default='Swimmer-v3')
+parser.add_argument('--horizon', help='Task horizon', type=int, default=500)
+parser.add_argument('--batchsize', help='Initial batch size', type=int, default=10)
+parser.add_argument('--network', help='Neural network size as space-separated integers', type=str, default="32 32")
+parser.add_argument('--activation', help='Neural network activation function', type=str, default="tanh")
+parser.add_argument('--iterations', help='Iterations', type=int, default=200)
+parser.add_argument('--disc', help='Discount factor', type=float, default=0.995)
 parser.add_argument('--std_init', help='Initial policy std', type=float, default=1.)
-parser.add_argument('--stepper', help='Step size rule', type=str, default='constant')
+parser.add_argument('--stepper', help='Step size rule', type=str, default='adam')
 parser.add_argument('--step', help='Step size', type=float, default=1e-3)
 parser.add_argument('--ent', help='Entropy bonus coefficient', type=float, default=0.)
 parser.add_argument("--render", help="Render an episode",
@@ -49,7 +53,7 @@ parser.add_argument("--learnstd", help="Learn std",
                     action="store_true")
 parser.add_argument("--no-learnstd", help="Don't learn std",
                     action="store_false")
-parser.set_defaults(render=False, temp=False, learnstd=False, test=False) 
+parser.set_defaults(render=False, temp=False, learnstd=True, test=False) 
 
 args = parser.parse_args()
 
@@ -58,18 +62,28 @@ args = parser.parse_args()
 env = gym.make(args.env)
 env.seed(args.seed)
 
-if type(env.action_space) is Discrete:
-    policy = ShallowGibbsPolicy(env, 
-                                temp=1.)
+hidden_neurons = [int(x) for x in args.network.split(" ")] if args.network else []
+if args.activation=="tanh":
+    activation = torch.tanh
+elif args.activation=="relu":
+    activation = torch.relu
 else:
-    m = sum(env.observation_space.shape)
-    d = sum(env.action_space.shape)
-    mu_init = torch.zeros(m*d)
-    logstd_init = torch.log(torch.zeros(d) + args.std_init)
-    policy = ShallowGaussianPolicy(m, d, 
-                               mu_init=mu_init, 
-                               logstd_init=logstd_init, 
-                               learn_std=args.learnstd)
+    raise NotImplementedError("Only available activation functions are tanh and relu")
+    
+m = sum(env.observation_space.shape)
+d = sum(env.action_space.shape)
+mu_init = torch.zeros(m*d)
+logstd_init = torch.log(torch.zeros(d) + args.std_init)
+
+policy = DeepGaussianPolicy(m, d,
+                           hidden_neurons=hidden_neurons,
+                           activation=activation,
+                           state_preproc=None,
+                           mu_init=None, #random initialization 
+                           logstd_init=logstd_init, 
+                           learn_std=args.learnstd,
+                           action_range=1.,
+                           )#init=partial(torch.nn.init.constant_))
 
 test_batchsize = args.batchsize if args.test else 0
 
@@ -91,7 +105,9 @@ else:
 
 
 # Run
+"""
 reinforce(env, policy,
+            action_filter = None,#lambda a: np.array((a,)),
             horizon = args.horizon,
             stepper = stepper,
             batchsize = args.batchsize,
@@ -105,4 +121,20 @@ reinforce(env, policy,
             estimator = args.estimator,
             baseline = args.baseline,
             test_batchsize=test_batchsize,
-            log_params=True)
+            log_params=False)
+"""
+stormpg(env, policy,
+            action_filter = None,#lambda a: np.array((a,)),
+            horizon = args.horizon,
+            stepper = stepper,
+            iterations = args.iterations,
+            disc = args.disc,
+            seed = args.seed,
+            logger = logger,
+            render = args.render,
+            shallow = False,
+            estimator = args.estimator,
+            baseline = args.baseline,
+            test_batchsize=test_batchsize,
+            log_params=False)
+#"""